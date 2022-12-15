ef75eed6c4f70494b1543264a2f80c6ba56f5171
Dayne Lalmond <dlalmondo@gmail.com>
2022-12-15 18:21:20 -0500

    readjusted package build script

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-//here
+//require auth
 const { authMiddleware } = require('./utils/auth');
 
 
