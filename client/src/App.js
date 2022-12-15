8fb115d58a5de865e5a47ed0b6d34a9a7f031501
Dayne Lalmond <dlalmondo@gmail.com>
2022-12-14 21:24:01 -0500

    added script for mac to run start

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -39,7 +16,6 @@ function App() {
           </div>
         </div>
       </Router>
->>>>>>> 3a02969473ef698b0251f4fc03b10a6b9e1280f4
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
@@ -26,4 +8,3 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root'));
->>>>>>> 3a02969473ef698b0251f4fc03b10a6b9e1280f4
